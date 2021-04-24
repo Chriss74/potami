4e29c38c937c1cb32429ab9a7a976aef9e418de3
Chriss74 <48810827+Chriss74@users.noreply.github.com>
2021-04-24 18:06:19 +0300

    Update rhta.js

--- rhta.js
+++ rhta.js
@@ -8,6 +8,45 @@ var voices=[];
 
 var count=0;
 
+function populateVoiceList() {
+    voices = synth.getVoices().sort(function (a, b) {
+        const aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
+        if ( aname < bname ) return -1;
+        else if ( aname == bname ) return 0;
+        else return +1;
+    });
+    
+    var selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+    voiceSelect.innerHTML = '';
+    for(i = 0; i < voices.length ; i++){
+    var option = document.createElement('option');
+    if(voices[i].name.includes('Greek')) {
+    var option = document.createElement('option');
+    option.textContent = 'Default-Greek  Male';
+    
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+    }}
+    for(i = 0; i < voices.length ; i++) {
+      var option = document.createElement('option');
+      option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+      
+      if(voices[i].name.includes('Greek')) {
+        option.textContent += ' -- DEFAULT';
+      }
+
+      
+  
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+    voiceSelect.selectedIndex = selectedIndex;
+  }
+  
+
+
 //TI THA LEEI ARXH
 const rhto = new SpeechSynthesisUtterance()
 
